9cd99091a1dd91902e1974f8106fdb3d5c2d9dfc
DavidWang <232190315@qq.com>
2015-11-17 17:25:24 +0800

    no message

--- app/src/main/java/tm/davidwang/dwprivatetraineranimation/MainActivity.java
+++ app/src/main/java/tm/davidwang/dwprivatetraineranimation/MainActivity.java
@@ -1,8 +1,8 @@
 package tm.davidwang.dwprivatetraineranimation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -14,8 +14,8 @@ import com.nineoldandroids.animation.ObjectAnimator;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private RelativeLayout explore_re,arrow_re,mianview,down_re;
-    final int duration = 400;
-    private ImageView card0,card1,card2,card3;
+    final int duration = 500;
+    private ImageView card0,card1,card2,card3,bgup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         explore_re = (RelativeLayout)findViewById(R.id.explore_re);
         arrow_re = (RelativeLayout)findViewById(R.id.arrow_re);
         mianview.setOnClickListener(this);
-
+        bgup = (ImageView)findViewById(R.id.bgup);
         card0 = (ImageView)findViewById(R.id.card0);
         card1 = (ImageView)findViewById(R.id.card1);
         card2 = (ImageView)findViewById(R.id.card2);
@@ -40,26 +40,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void inData(){
         ObjectAnimator.ofFloat(arrow_re, "translationY", 0, dip2px(85)).start();
+        ObjectAnimator.ofFloat(bgup, "translationY", 0, dip2px(33) - 1280).start();
         ObjectAnimator.ofFloat(explore_re, "scaleX", 1, 0).start();
         ObjectAnimator.ofFloat(card0, "translationX", 0, dip2px(430)).start();
         ObjectAnimator.ofFloat(card1, "translationX", 0, dip2px(450)).start();
         ObjectAnimator.ofFloat(card2, "translationX", 0, dip2px(470)).start();
         ObjectAnimator.ofFloat(card3, "translationX", 0, dip2px(490)).start();
-
+        ObjectAnimator.ofFloat(card0, "scaleX", 1, 0.7f).start();
+        ObjectAnimator.ofFloat(card1, "scaleX", 1, 0.7f).start();
+        ObjectAnimator.ofFloat(card2, "scaleX", 1, 0.7f).start();
+        ObjectAnimator.ofFloat(card3, "scaleX", 1, 0.7f).start();
         new Handler().postDelayed(new Runnable() {
             public void run() {
+                showView();
                 Bottom();
                 Top();
             }
-        }, 1000);
+        }, 300);
 
+    }
+
+    private void showView(){
+        down_re.setVisibility(View.VISIBLE);
+        bgup.setVisibility(View.VISIBLE);
+        explore_re.setVisibility(View.VISIBLE);
+        arrow_re.setVisibility(View.VISIBLE);
+        card0.setVisibility(View.VISIBLE);
+        card1.setVisibility(View.VISIBLE);
+        card2.setVisibility(View.VISIBLE);
+        card3.setVisibility(View.VISIBLE);
     }
 
     private void Top(){
         ObjectAnimator.ofFloat(card0, "translationX", dip2px(430), 0).setDuration(duration*2 - 400).start();
+        ObjectAnimator.ofFloat(card0, "scaleX", 0.7f,1).setDuration(duration * 2 - 400).start();
         ObjectAnimator.ofFloat(card1, "translationX", dip2px(450), 0).setDuration(duration*2 - 300).start();
+        ObjectAnimator.ofFloat(card1, "scaleX", 0.7f,1).setDuration(duration*2 - 300).start();
         ObjectAnimator.ofFloat(card2, "translationX", dip2px(470), 0).setDuration(duration*2 - 200).start();
+        ObjectAnimator.ofFloat(card2, "scaleX", 0.7f,1).setDuration(duration*2 - 200).start();
         ObjectAnimator.ofFloat(card3, "translationX", dip2px(490), 0).setDuration(duration*2 - 100).start();
+        ObjectAnimator.ofFloat(card3, "scaleX", 0.7f,1).setDuration(duration*2 - 100).start();
+        ObjectAnimator.ofFloat(bgup, "translationY",dip2px(33) - 1280, 0).setDuration(duration*2 - 100).start();
     }
 
     private void Bottom(){
@@ -67,7 +88,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         set.playTogether(
                 ObjectAnimator.ofFloat(arrow_re, "translationY", dip2px(85), 0)
         );
-        set.setDuration(duration).start();
+        set.setDuration(duration*2 - 200).start();
         set.addListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animation) {
@@ -112,8 +133,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case  R.id.mianview:
-                Bottom();
-                Top();
+                ObjectAnimator.ofFloat(arrow_re, "translationX",dip2px(83),0).start();
+                ObjectAnimator.ofFloat(arrow_re, "scaleX",0.6f,1).start();
+                ObjectAnimator.ofFloat(arrow_re, "scaleY",0.6f,1).start();
+                inData();
                 break;
         }
 
